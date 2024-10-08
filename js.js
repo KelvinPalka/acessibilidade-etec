a4a7f8923c55ecbe3fbd79445b4005f558a98fd4
theo-souza-43 <theo.souza122@gmail.com>
2024-10-07 21:11:08 -0300

    Update js.js
    daltonismo no js externo

--- js.js
+++ js.js
@@ -12,3 +12,39 @@ jQuery(document).ready(function(){
         return false;
     });
 });
+
+// bglh de deficiente de cor dos óio
+
+function toggleDropdown() {
+var dropdown = document.getElementById("colorDropdown");
+var button = document.getElementById("accessibilityBtn");
+dropdown.classList.toggle("show");
+button.classList.toggle("active");
+}
+
+function applyFilter(type) {
+var htmlElement = document.documentElement;
+var filter = "none";
+switch (type) {
+    case "protanopia":
+    filter = "url(#protanopia-filter)";
+    break;
+    case "deuteranopia":
+    filter = "url(#deuteranopia-filter)";
+    break;
+    case "tritanopia":
+    filter = "url(#tritanopia-filter)";
+    break;
+}
+htmlElement.style.filter = filter;
+toggleDropdown();
+}
+
+document.addEventListener("click", function (event) {
+var dropdown = document.getElementById("colorDropdown");
+var button = document.getElementById("accessibilityBtn");
+if (!event.target.closest(".accessibility-wrapper")) {
+    dropdown.classList.remove("show");
+    button.classList.remove("active");
+}
+});
